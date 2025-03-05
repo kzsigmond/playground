768e1ec5d86cba894433156e61710c2274fb54e8
Krisztina Zsigmond <144738961+kzsigmond@users.noreply.github.com>
2025-03-05 15:57:15 -0500

    Add docstring to code.

--- playground/add.py
+++ playground/add.py
@@ -1,2 +1,11 @@
 def add(a, b):
+    """Add two integer numbers.
+    
+    Args:
+        a (int): First number.
+        b (int): Second number.
+
+    Returns:
+        int: The sum of a and b.
+    """
     return a+b
