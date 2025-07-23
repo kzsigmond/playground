b3ecb6e51e83aadc924eb993a6b4f25e7fc4396e
Rugwed Lokhande <101607593+rugwed-lokhande@users.noreply.github.com>
2025-07-23 11:34:49 -0400

    Create multiply.py

---
+++ playground/multiply.py
@@ -0,0 +1,11 @@
+def multiply(a, b):
+    """multiply two integer numbers.
+    
+    Args:
+        a (int): First number.
+        b (int): Second number.
+
+    Returns:
+        int: The sum of a and b.
+    """
+    return a*b
